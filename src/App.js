ec6aa24c2516ac4391186fb7de3433ff25512a56
Suraj Bhattacharya <44540215+SurajBong@users.noreply.github.com>
2020-02-22 00:23:52 +0530

    Added App.test cases

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      error: false
+    };
+  }
+
+  decrementCounter = () => {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  };
+
+  incrementCounter = () => {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  render() {
+    const errorClass = this.state.error ? "" : "hidden";
+    return (
+      <div className="App" data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        <div data-test="error-message" className={`error ${errorClass}`}>
+          The counter cannot go below 0
+        </div>
+        <button data-test="increment-button" onClick={this.incrementCounter}>
+          Increment counter
+        </button>
+        <button data-test="decrement-button" onClick={this.decrementCounter}>
+          Decrement counter
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
